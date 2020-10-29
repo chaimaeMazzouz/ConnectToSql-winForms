a66e13e60f87f2ea247ebca0db04fe4e6b9e601d
chaimaeMazzouz <mazzouz.chaimae.dev@gmail.com>
2020-10-29 20:52:06 +0100

    Connect To Sql using C# WinnForms Applications

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace connectToSql_winForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTest = new System.Windows.Forms.Label();
+            this.btnConnecter = new System.Windows.Forms.Button();
+            this.btnDeconnecter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTest.Location = new System.Drawing.Point(45, 9);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(206, 26);
+            this.lblTest.TabIndex = 0;
+            this.lblTest.Text = "Test de connexion";
+            // 
+            // btnConnecter
+            // 
+            this.btnConnecter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnecter.ForeColor = System.Drawing.Color.LimeGreen;
+            this.btnConnecter.Location = new System.Drawing.Point(17, 56);
+            this.btnConnecter.Name = "btnConnecter";
+            this.btnConnecter.Size = new System.Drawing.Size(118, 32);
+            this.btnConnecter.TabIndex = 1;
+            this.btnConnecter.Text = "Connecter";
+            this.btnConnecter.UseVisualStyleBackColor = true;
+            this.btnConnecter.Click += new System.EventHandler(this.btnConnecter_Click);
+            // 
+            // btnDeconnecter
+            // 
+            this.btnDeconnecter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeconnecter.ForeColor = System.Drawing.Color.Red;
+            this.btnDeconnecter.Location = new System.Drawing.Point(154, 56);
+            this.btnDeconnecter.Name = "btnDeconnecter";
+            this.btnDeconnecter.Size = new System.Drawing.Size(124, 32);
+            this.btnDeconnecter.TabIndex = 2;
+            this.btnDeconnecter.Text = "Déconnecter";
+            this.btnDeconnecter.UseVisualStyleBackColor = true;
+            this.btnDeconnecter.Click += new System.EventHandler(this.btnDeconnecter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 105);
+            this.Controls.Add(this.btnDeconnecter);
+            this.Controls.Add(this.btnConnecter);
+            this.Controls.Add(this.lblTest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTest;
+        private System.Windows.Forms.Button btnConnecter;
+        private System.Windows.Forms.Button btnDeconnecter;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace connectToSql_winForm
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection oConnection;
+        public Form1()
+        {
+            InitializeComponent();
+            string strConnection = @"data source =DESKTOP-7TC9CTO\SQLEXPRESS;database=VolAvion;Integrated Security=True";
+            oConnection = new SqlConnection(strConnection);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConnecter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                oConnection.Open();
+                lblTest.Text = oConnection.State.ToString();
+                lblTest.ForeColor = Color.Green;
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("l'erreur suivante a été rencontrée : " + exp.Message);
+            }
+           
+        }
+
+        private void btnDeconnecter_Click(object sender, EventArgs e)
+        {
+            oConnection.Close();
+            lblTest.Text = oConnection.State.ToString();
+            lblTest.ForeColor = Color.Red;
+        }
+    }
+}
